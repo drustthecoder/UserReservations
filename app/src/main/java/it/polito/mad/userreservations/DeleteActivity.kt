965d9196ec6a099d9a95bdbbc666d3c747de6403
Shayan Taghinezhad Roudbaraki s301425 <s301425@studenti.polito.it>
2023-04-17 22:57:37 +0200

    Modify and delete activity added.

---
+++ app/src/main/java/it/polito/mad/userreservations/DeleteActivity.kt
@@ -0,0 +1,11 @@
+package it.polito.mad.userreservations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete)
+    }
+}
--- app/src/main/java/it/polito/mad/userreservations/ReservationsActivity.kt
+++ app/src/main/java/it/polito/mad/userreservations/ReservationsActivity.kt
@@ -1,7 +1,9 @@
 package it.polito.mad.userreservations
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +11,15 @@ class ReservationsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservations)
+
+        val modifyBtn = findViewById<Button>(R.id.modifyBtn)
+        val deleteBtn = findViewById<Button>(R.id.deleteBtn)
+
+        modifyBtn.setOnClickListener(){
+            startActivity(Intent(this, ModifyActivity::class.java))
+        }
+        deleteBtn.setOnClickListener(){
+            startActivity(Intent(this, DeleteActivity::class.java))
+        }
     }
 }
